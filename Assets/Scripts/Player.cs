4a22efc5f6f99807d77fc5356adef36308133a2e
ryan2414 <jackpar@nate.com>
2023-08-27 18:54:52 +0900

    add map & Enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    Vector3 v3;
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+
+    public Vector2 inputVec;
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriter;
+    private Animator animator;
+
+    private void Awake()
     {
-                
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+
     void Update()
     {
-        
+        // Old Input System
+        //inputVec.x = Input.GetAxisRaw("Horizontal");
+        //inputVec.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Old version
+        //Vector2 nextVec = inputVec.normalized * speed * Time.deltaTime;
+
+        Vector2 nextVec = inputVec * speed * Time.deltaTime; 
+        rigid.MovePosition(rigid.position + nextVec);
+
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        inputVec = inputValue.Get<Vector2>();
     }
 }
